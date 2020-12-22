11759343057fd4ef8e2e3a28bb554132d9feb688
Magiske <nastya.borzenko10@mail.ru>
2020-12-22 20:13:22 +0700

    готово

--- HelloGIT/Program.cs
+++ HelloGIT/Program.cs
@@ -10,6 +10,8 @@ namespace HelloGIT
     {
         static void Main(string[] args)
         {
+            int A = 0, B = 0, choice;
+
             Console.WriteLine("1. Ввести число A");
             Console.WriteLine("2. Ввести число B");
             Console.WriteLine("3. Сложить A и B");
@@ -17,8 +19,38 @@ namespace HelloGIT
             Console.WriteLine("5. Умножить A на B");
             Console.WriteLine("6. Разделить A на B");
 
-            Console.Write("Введите номер пункта меню: ");
+            for ( ; ; )
+            {
+                Console.Write("Введите номер пункта меню (или другое число, если хотите выйти): ");
+                choice = Convert.ToInt32(Console.ReadLine());
 
+                if (choice == 1)
+                {
+                    A = Enter_A();
+                }
+                else if (choice == 2)
+                {
+                    B = Enter_B();
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("Число A = {0}, число B = {1}, результат сложения = {2}", A, B, A+B);
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine("Число A = {0}, число B = {1}, результат вычитания B из A = {2}", A, B, A - B);
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("Число A = {0}, число B = {1}, результат умножения = {2}", A, B, A * B);
+                }
+                else if (choice == 6)
+                {
+                    Console.WriteLine("Число A = {0}, число B = {1}, результат деления A на B = {2}", A, B, A / B);
+                }
+                else
+                    break;
+            }
 
             Console.ReadKey();
         }
