8f58acd95e4a7558bcc68d8c17acf6d01701e1cb
Magiske <nastya.borzenko10@mail.ru>
2020-12-22 18:27:25 +0700

    финальная ветка

--- HelloGIT/Program.cs
+++ HelloGIT/Program.cs
@@ -10,21 +10,16 @@ namespace HelloGIT
     {
         static void Main(string[] args)
         {
-            int A;
+            Console.WriteLine("1. Ввести число A");
+            Console.WriteLine("2. Ввести число B");
+            Console.WriteLine("3. Сложить A и B");
+            Console.WriteLine("4. Вычесть B из A");
+            Console.WriteLine("5. Умножить A на B");
+            Console.WriteLine("6. Разделить A на B");
 
-            Console.WriteLine("Hello GIT!");
+            Console.Write("Введите номер пункта меню: ");
 
-            Console.Write("Введите A: ");
-            A = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("\nНажмите любую клавишу, дотвидания");
             Console.ReadKey();
-        }
-
-        static int Enter_A()
-        {
-            Console.Write("Введите A: ");
-            return Convert.ToInt32(Console.ReadLine());
         }
     }
 }
