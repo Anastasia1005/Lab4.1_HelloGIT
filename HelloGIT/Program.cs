5dd79fa5d061749e140ed20e7efc4493f57eeddb
Magiske <nastya.borzenko10@mail.ru>
2020-12-22 18:07:52 +0700

    add "final" branch

--- HelloGIT/Program.cs
+++ HelloGIT/Program.cs
@@ -10,6 +10,15 @@ namespace HelloGIT
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1. Ввести число A");
+            Console.WriteLine("2. Ввести число B");
+            Console.WriteLine("3. Сложить A и B");
+            Console.WriteLine("4. Вычесть B из A");
+            Console.WriteLine("5. Умножить A на B");
+            Console.WriteLine("6. Разделить A на B");
+
+            Console.Write("Введите номер пункта меню: ");
+
             Console.ReadKey();
         }
     }
